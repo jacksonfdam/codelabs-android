b5c429aa89e04366695309da9f5bef7474ef3ab0
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2020-09-14 16:38:30 -0300

    - Codelab 11 - Data Binding Library

---
+++ Codelab11/app/src/main/java/com/jacksonfdam/codelabs/codelab11/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.jacksonfdam.codelabs.codelab11
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import androidx.databinding.DataBindingUtil
+import com.jacksonfdam.codelabs.codelab11.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private val myName: MyName = MyName("Jackson Mafra")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.myName = myName
+
+        binding.doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    /**
+     * Click handler for the Done button.
+     * Demonstrates how data binding can be used to make code much more readable
+     * by eliminating calls to findViewById and changing data in the binding object.
+     */
+    private fun addNickname(view: View) {
+        binding. apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            doneButton.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+        // Hide the keyboard.
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
+    }
+}
+
+data class MyName(var name: String = "", var nickname: String = "")
